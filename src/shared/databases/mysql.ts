bc4eea5a6c539a14b9bf4e9c431003f1bc385fc9
Leo <leonelantonio569@gmail.com>
2021-10-01 15:24:28 -0300

    Path of models fixed 2

--- src/shared/databases/mysql.ts
+++ src/shared/databases/mysql.ts
@@ -8,7 +8,7 @@ const connectionConf: ConnectionOptions = {
   username: process.env.DB_USERNAME ?? '',
   password: process.env.DB_PASSWORD ?? '',
   database: process.env.DB_DATABASE ?? '',
-  entities: [ 'models/**/*.js' ],
+  entities: [ 'models/*' ],
   synchronize: false,
   logging: true,
 }
